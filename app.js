5e4624a1920c270cba19343a8177213abedb5d7a
Pablo Gutierrez <pablo.gutierrez.munoz@gmail.com>
2022-04-16 17:34:14 +0200

    añadidos scrollReveals

---
+++ app.js
@@ -0,0 +1,37 @@
+ScrollReveal({ duration: 2000 });
+ScrollReveal({ distance: '200px' });
+ScrollReveal({ easing: 'cubic-bezier(0.5, 0, 0, 1)' });
+ScrollReveal().reveal('.h1', { origin: 'left' });
+ScrollReveal().reveal('.h1', { easing: 'ease-in' });
+ScrollReveal().reveal('.h2', {delay : 200});
+ScrollReveal().reveal('.h2', { origin: 'right' });
+ScrollReveal().reveal('.h2', { easing: 'ease-in' });
+ScrollReveal().reveal('.btn', { delay: 200 });
+ScrollReveal().reveal('.btn', { easing: 'ease-in' });
+
+ScrollReveal().reveal('.imgSeccion1', { easing: 'ease-in' });
+ScrollReveal().reveal('.textSeccion1 h3', {delay : 150});
+ScrollReveal().reveal('.textSeccion1 h3', { easing: 'ease-in' });
+ScrollReveal().reveal('.textSeccion1 h3', { origin: 'right' });
+ScrollReveal().reveal('.textSeccion1 p', {delay : 150});
+ScrollReveal().reveal('.textSeccion1 p', { easing: 'ease-in' });
+
+ScrollReveal().reveal('.seccion2', { easing: 'ease-in' });
+
+ScrollReveal().reveal('.seccion3 ul', { easing: 'ease-in' });
+ScrollReveal().reveal('.imgSeccion3', {delay : 150});
+ScrollReveal().reveal('.imgSeccion3', { easing: 'ease-in' });
+ScrollReveal().reveal('.imgSeccion3', { origin: 'right' });
+ScrollReveal().reveal('.img2Seccion3', {delay : 150});
+ScrollReveal().reveal('.img2Seccion3', { easing: 'ease-in' });
+ScrollReveal().reveal('.img2Seccion3', { origin: 'right' });
+
+ScrollReveal().reveal('.seccion4', { easing: 'ease-in-out' });
+
+
+
+
+
+
+
+
