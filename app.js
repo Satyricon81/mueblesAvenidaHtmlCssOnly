4f77264793e45e90b86976455f3e075dac3db787
Pablo Gutierrez <pablo.gutierrez.munoz@gmail.com>
2022-04-16 17:48:57 +0200

    añadidos scrollReveals 80%

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ ScrollReveal({ distance: '200px' });
 ScrollReveal({ easing: 'cubic-bezier(0.5, 0, 0, 1)' });
 ScrollReveal().reveal('.h1', { origin: 'left' });
 ScrollReveal().reveal('.h1', { easing: 'ease-in' });
-ScrollReveal().reveal('.h2', {delay : 200});
+ScrollReveal().reveal('.h2', {delay : 100});
 ScrollReveal().reveal('.h2', { origin: 'right' });
 ScrollReveal().reveal('.h2', { easing: 'ease-in' });
-ScrollReveal().reveal('.btn', { delay: 200 });
-ScrollReveal().reveal('.btn', { easing: 'ease-in' });
+ScrollReveal().reveal('.h2', { origin: 'top' });
+ScrollReveal().reveal('.scroll', { delay: 150 });
+ScrollReveal().reveal('.scroll', { easing: 'ease-in' });
 
 ScrollReveal().reveal('.imgSeccion1', { easing: 'ease-in' });
-ScrollReveal().reveal('.textSeccion1 h3', {delay : 150});
+ScrollReveal().reveal('.textSeccion1 h3', {delay : 50});
 ScrollReveal().reveal('.textSeccion1 h3', { easing: 'ease-in' });
 ScrollReveal().reveal('.textSeccion1 h3', { origin: 'right' });
-ScrollReveal().reveal('.textSeccion1 p', {delay : 150});
-ScrollReveal().reveal('.textSeccion1 p', { easing: 'ease-in' });
 
-ScrollReveal().reveal('.seccion2', { easing: 'ease-in' });
+ScrollReveal().reveal('.cards', {delay : 50});
+ScrollReveal().reveal('.cards', { easing: 'ease-in' });
+ScrollReveal().reveal('.cards', { origin: 'bottom' });
 
-ScrollReveal().reveal('.seccion3 ul', { easing: 'ease-in' });
+ScrollReveal().reveal('.seccion3 ul', {delay : 50});
+ScrollReveal().reveal('.seccion3 ul', { easing: 'ease-in-out' });
+ScrollReveal().reveal('.seccion3 ul', { origin: 'left' });
+
 ScrollReveal().reveal('.imgSeccion3', {delay : 150});
 ScrollReveal().reveal('.imgSeccion3', { easing: 'ease-in' });
 ScrollReveal().reveal('.imgSeccion3', { origin: 'right' });
@@ -26,7 +30,7 @@ ScrollReveal().reveal('.img2Seccion3', {delay : 150});
 ScrollReveal().reveal('.img2Seccion3', { easing: 'ease-in' });
 ScrollReveal().reveal('.img2Seccion3', { origin: 'right' });
 
-ScrollReveal().reveal('.seccion4', { easing: 'ease-in-out' });
+ScrollReveal().reveal('.seccion4 h2', { easing: 'ease-in-out' });
 
 
 
